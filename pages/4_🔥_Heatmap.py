321a92140ea4c798841cc979482e9108702fe5f8
GlenYali <102636664+GlenYali@users.noreply.github.com>
2022-08-12 02:11:37 +1000

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -20,11 +20,11 @@
         #filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
        # m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
        # m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="pop_max",
-            name="Heat map",
-            radius=20,
+           # filepath,
+           # latitude="latitude",
+          #  longitude="longitude",
+           # value="pop_max",
+          #  name="Heat map",
+           # radius=20,
         )
 #m.to_streamlit(height=700)
